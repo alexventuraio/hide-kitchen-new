f1072a5346ca7388de5a07f79581216fc936adf9
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2021-03-27 08:24:44 -0600

    Add input types to the safelist when purging classes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 module.exports = {
-  purge: [
-    './app/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './src/**/*.html',
-    './src/**/*.js',
-    './src/**/*.jsx',
-  ],
+  purge: {
+    content: [
+      './app/**/*.html.erb',
+      './app/helpers/**/*.rb',
+      './app/components/**/*.html.erb',
+      './app/components/**/*.rb',
+      './app/javascript/**/*.js',
+    ],
+
+    // These options are passed through directly to PurgeCSS
+    options: {
+      safelist: ['bg-red-500', 'px-4'],
+      // This is what I needed to get forms / inputs styled correctly. It adds the types selectors on production.
+      safelist: [
+        [type='text'], [type='email'], [type='url'], [type='password'], [type='number'], [type='date'], [type='search'], [type='time'], [type='checkbox'], [type='radio']
+      ],
+    },
+  },
 
   darkMode: false, // or 'media' or 'class'
   theme: {
