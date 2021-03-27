c2cb2a7bbfba2e778cf1893c429adf8f7752d88d
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2021-03-27 08:38:34 -0600

    Fix double safelist declaration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,6 @@ module.exports = {
 
     // These options are passed through directly to PurgeCSS
     options: {
-      safelist: ['bg-red-500', 'px-4'],
       // This is what I needed to get forms / inputs styled correctly. It adds the types selectors on production.
       safelist: [
         [type='text'], [type='email'], [type='url'], [type='password'], [type='number'], [type='date'], [type='search'], [type='time'], [type='checkbox'], [type='radio']
